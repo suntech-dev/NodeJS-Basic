cfc003bed3e66d543947aa16d8cf87484da81129
dev@suntech.asia <Young0123!>
2021-07-15 18:49:53 +0900

    password protect

---
+++ config/prod.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: process.env.MONGO_URI, // MONGO_URI => 'HEROQU'
+};
--- index.js
+++ index.js
@@ -1,23 +1,43 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+const { User } = require('./models/User');
+
+// application/x-www~from-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
 mongoose
-    .connect(
-        'mongodb+srv://dev:suntech9304!@nodejs-01.lcdn9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-        {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-            useFindAndModify: false,
-        }
-    )
+    .connect(config.mongoURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+    })
     .then(() => console.log(`MongoDB Connected...`))
     .catch((err) => console.log(`Connect Error...`));
 
 app.get('/', (req, res) => {
-    res.send('SUNTECH KOREA!');
+    res.send('SUNTECH KOREA!!!');
+});
+
+app.post('/register', (req, res) => {
+    // sign in info --> insert DB
+
+    const user = new User(req.body);
+
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err });
+        return res.status(200).json({
+            success: true,
+        });
+    });
 });
 
 app.listen(port, () => {
